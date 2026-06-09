8eeaf9937f2d0d4c4ae80e68dcc75c4ec4902a1c
Rangon Roy Utsab <rangonroy.rzs17@gmail.com>
2026-06-09 08:10:37 +0600

    util: added validator

--- utils/validation.go
+++ utils/validation.go
@@ -1,1 +1,46 @@
 package utils
+
+import (
+	"errors"
+	"strings"
+
+	"travel-sphere-rangon/models"
+)
+
+func ValidateCountryName(countryName string) error {
+	if strings.TrimSpace(countryName) == "" {
+		return errors.New("country name is required")
+	}
+
+	return nil
+}
+
+func ValidateWishlistStatus(status string) error {
+	status = strings.TrimSpace(status)
+
+	if status != models.StatusPlanned && status != models.StatusVisited {
+		return errors.New("status must be Planned or Visited")
+	}
+
+	return nil
+}
+
+func ValidateWishlistID(id string) error {
+	if strings.TrimSpace(id) == "" {
+		return errors.New("wishlist item id is required")
+	}
+
+	return nil
+}
+
+func ValidateCoordinates(lat float64, lng float64) error {
+	if lat < -90 || lat > 90 {
+		return errors.New("latitude must be between -90 and 90")
+	}
+
+	if lng < -180 || lng > 180 {
+		return errors.New("longitude must be between -180 and 180")
+	}
+
+	return nil
+}
